5db25a67e0e2ff25eb5d4a59008bf2837c839982
Muhammad Faizan Fayyaz <fayyaz@myseneca.ca>
2026-04-07 22:44:49 -0400

    Fix department.h to define name as an array

--- department.h
+++ department.h
@@ -6,4 +6,22 @@
 class Department
 {
 private:
-    char name
+    char name[100];
+    Product* products;
+    int count;
+
+public:
+    Department();
+    ~Department();
+    
+    void setDepartmentName(const char*);
+    const char* getDepartmentName() const;
+
+    void addProduct(const Product&);
+    void listProducts() const;
+
+    Product* getProducts() const;
+    int geTotalProducts() const;
+};    
+
+#endif
