0535f02f311646fe60a6f35ece162cfcb4fd774a
khankamraan2006-crypto <khankamraan2006@gmail.com>
2026-04-07 22:29:27 -0400

    Refactor Department class for product handling
    Fix memory management and product listing in Department class.

--- department.cpp
+++ department.cpp
@@ -1,1 +1,41 @@
+#include "department.h"
+#include <iosteam>
+#include <cstring>
+using namespace std;
 
+Department::Department()
+{
+  name[0]='\0';
+  product=0;
+  count=0;
+}
+Department::~Department()
+{
+  delete[] products;
+}
+void Department::setDepartmentName(const char*n)
+{
+  strncpy(name,n,99);
+  name[99]='\0'
+}
+const char* Department::getDepartmentName() const
+{
+  return name;
+}
+void Department::addProduct(const Product& p)
+{
+  Product* temp=new Product[count+1];
+  for (int i=0;i<count;i++)
+    temp[i]=product[i];
+  temp[count]=p;
+  delete[] products;
+  products=temp;
+  count++;
+}
+void Department::listProducts() const
+{
+  for(int i=0;i<count;i++)
+    {
+      cout<<i+1<<"."<<
+    }
+}
