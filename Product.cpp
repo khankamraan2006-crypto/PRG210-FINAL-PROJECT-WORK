e5eff783203cd566e323b9a1c8f3143765c6196e
khankamraan2006-crypto <khankamraan2006@gmail.com>
2026-04-11 19:09:33 -0400

    Refactor Product class constructors and methods

--- Product.cpp
+++ Product.cpp
@@ -1,11 +1,50 @@
 #include "Product.h"
 #include <cstring>
 
-Product::(){
-  name[0] = '\0'
-  price = 0;
-  quantity = 0;
+Product::Product()
+{
+    name[0] = '\0';
+    price = 0.0;
+    quantity = 0;
 }
-Product::Product(const char*n, double p, int q){
-  
+
+Product::Product(const char* itemName, double itemPrice, int itemQuantity)
+{
+    setProduct(itemName, itemPrice, itemQuantity);
+}
+
+void Product::setProduct(const char* itemName, double itemPrice, int itemQuantity)
+{
+    if (itemName != 0)
+    {
+        strncpy(name, itemName, 99);
+        name[99] = '\0';
+    }
+    else
+    {
+        name[0] = '\0';
+    }
+
+    price = itemPrice;
+    quantity = itemQuantity;
+}
+
+const char* Product::getName() const
+{
+    return name;
+}
+
+double Product::getPrice() const
+{
+    return price;
+}
+
+int Product::getQuantity() const
+{
+    return quantity;
+}
+
+void Product::setQuantity(int itemQuantity)
+{
+    quantity = itemQuantity;
 }
