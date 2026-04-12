ed0725681911618139b02c5fb8d218b08ba3337d
Muhammad Faizan Fayyaz <fayyaz@myseneca.ca>
2026-04-12 13:42:23 -0400

    Define Interface class with virtual methods

--- Interface.h
+++ Interface.h
@@ -1,1 +1,17 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
 
+class Interface
+{
+public:
+    virtual void showMenu() = 0;
+    virtual ~Interface();
+
+protected:
+    int readIntInRange(int min, int max);
+    int readPositiveInt();
+    double readPositiveDouble();
+    void readNonEmptyString(char* text, int size);
+};
+
+#endif
